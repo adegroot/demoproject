6fec3d52edffebb4a9c4a7609cc567d9146bc532
ari <adegroot@gmail.com>
2018-11-07 20:20:20 +0100

    init

---
+++ src/main/java/com/example/demo/SuperDuperClass.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class SuperDuperClass {
+
+	public String getMessage() {
+		return "wooow";
+	}
+	
+	public String getMessage2() {
+		return "wooow";
+	}
+
+	
+}
